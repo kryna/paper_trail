34bdb39d33382df8c1b858998ad8f47078ba3e9f
kryna <krynicki.krzysiek@gmail.com>
2015-04-03 12:30:35 +0200

    Added custom methods

--- lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
+++ lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
@@ -3,5 +3,13 @@ require 'paper_trail/version_concern'
 module PaperTrail
   class Version < ::ActiveRecord::Base
     include PaperTrail::VersionConcern
+
+    def id_serialized
+      "#{id}&PaperTrail::Version"
+    end
+
+    def version_date
+      self.reify.updated_at.try(:strftime, "%d.%m.%Y %H:%M")
+    end
   end
 end
